aa791832659aa1a084156e664e2a286ee37e1ba4
ElmerGama <43096999+ElmerGama@users.noreply.github.com>
2024-10-23 11:35:48 +0000

    Primer commit, agregación de componentes

---
+++ src/app/segundocomponente/segundocomponente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-segundocomponente',
+  standalone: true,
+  imports: [],
+  templateUrl: './segundocomponente.component.html',
+  styleUrl: './segundocomponente.component.css'
+})
+export class SegundocomponenteComponent {
+
+}
